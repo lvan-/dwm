d0145cfb864cab3d084693d299a465edda134897
ivan o <oyamaivan@gmail.com>
2020-05-09 21:22:26 -0300

    atalhosvolume

--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-
+#include <X11/XF86keysym.h>
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -59,6 +59,10 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 
+static const char *mutecmd[] = { "volume.sh", "toggle", NULL };
+static const char *volupcmd[] = { "volume.sh", "up", NULL };
+static const char *voldowncmd[] = { "volume.sh", "down", NULL };
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -94,6 +98,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { 0, XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd} },
+    { 0, XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd} },
+    { 0, XF86XK_AudioMute,        spawn, {.v = mutecmd} },
 };
 
 /* button definitions */
