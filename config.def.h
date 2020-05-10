18fc806972b569d87d8a7a4f0f983f6f931762a8
ivan o <oyamaivan@gmail.com>
2020-05-09 23:47:33 -0300

    cores

--- config.def.h
+++ config.def.h
@@ -1,21 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_fg[]          = "#e9e9e9";
+static const char col_bg[]          = "#0f0b00";
+static const char col_black[]       = "#282828";
+static const char col_grey[]        = "#e9e9e9";
+static const char col_red[]         = "#da1657";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_grey,  col_bg,    col_bg },
+	[SchemeSel]  = { col_bg,    col_fg,    col_fg  },
 };
 
 /* tagging */
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_fg, "-nf", col_bg, "-sb", col_red, "-sf", col_fg, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 
 static const char *mutecmd[] = { "volume.sh", "toggle", NULL };
